d4aae30461293827ff53bb5a8d9ab0daf2a8f24e
Massimiliano Giunchi <massimiliano.giunchi@gmail.com>
2020-12-30 17:11:48 +0100

    Added register.html, login.html, forms.py class

--- src/flaskBlog.py
+++ src/flaskBlog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from src.forms import RegistrationForm, LoginForm
 app = Flask(__name__) #var
+
+#Set secret  cookie
+app.config['SECRET_KEY']='e4f3b857d3c940c8a2683afa6055ad29'
 
 posts = [
     {
@@ -18,12 +22,32 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html',posts = posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='about')
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Accont created for {form.username.data}!','success')
+        return redirect(url_for('home')) #redirect to the function
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Check your username and password','danger')
+    return render_template('login.html', title='Login', form=form)
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
---
+++ src/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    #Username is name of field
+    username = StringField('Username', validators=[DataRequired(),Length(min=2,max=50)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign Up')
