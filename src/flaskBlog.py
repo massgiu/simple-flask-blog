563575296bbddc32d4b334c40528765769c4465f
Massimiliano Giunchi <massimiliano.giunchi@gmail.com>
2020-12-31 15:46:51 +0100

    Created SQLlite DB with 2 tables: Post and User

--- src/flaskBlog.py
+++ src/flaskBlog.py
@@ -1,9 +1,40 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from src.forms import RegistrationForm, LoginForm
 app = Flask(__name__) #var
 
 #Set secret  cookie
-app.config['SECRET_KEY']='e4f3b857d3c940c8a2683afa6055ad29'
+app.config['SECRET_KEY'] = 'e4f3b857d3c940c8a2683afa6055ad29'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # /// relative path (site.db is db file)
+
+db = SQLAlchemy(app) #instance of DB
+
+#define DB structure
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    #This is a relationship non a field, but I can query user.posts (launch a query)
+    #author field is linked with user_id to User table
+    posts = db.relationship('Post', backref='author', lazy=True) #the user can create many posts, but every post has one user
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) #we pass function and non current time datetime.utcnow()
+    content = db.Column(db.Text, nullable=False)
+    #This fiel is the primary key of User Table
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),  nullable=False) #user.id is the name of field
+
+    #This method prints the obj representation
+    def __repr__(self):
+        return f"User('{self.title}','{self.date_posted}')"
 
 posts = [
     {
